341181a2c2c0a1d04677e4316f8c39b0865c38c8
vishnubansal001 <vishnu1549.be21@chitkara.edu.in>
2023-06-26 23:43:36 +0530

    car component added

--- utils/index.ts
+++ utils/index.ts
@@ -1,16 +1,41 @@
-const url = 'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla';
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '264fadaf53msh27d1d5cc9d4b9ddp177687jsn9625a31ccec9',
-		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
-	}
-};
+import { FilterProps } from "@/types";
 
-try {
-	// const response = await fetch(url, options);
-	// const result = await response.text();
-	// console.log(result);
-} catch (error) {
-	console.error(error);
-}
+// const url = 'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla';
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': '264fadaf53msh27d1d5cc9d4b9ddp177687jsn9625a31ccec9',
+// 		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
+// 	}
+// };
+
+// try {
+// 	// const response = await fetch(url, options);
+// 	// const result = await response.text();
+// 	// console.log(result);
+// } catch (error) {
+// 	console.error(error);
+// }
+
+export async function fetchCars(filters: FilterProps) {
+	const { manufacturer, year, model, limit, fuel } = filters;
+  
+	// Set the required headers for the API request
+	const headers: HeadersInit = {
+	  "X-RapidAPI-Key": "264fadaf53msh27d1d5cc9d4b9ddp177687jsn9625a31ccec9",
+	  "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
+	};
+  
+	// Set the required headers for the API request
+	const response = await fetch(
+	  `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`,
+	  {
+		headers: headers,
+	  }
+	);
+  
+	// Parse the response as JSON
+	const result = await response.json();
+  
+	return result;
+  }
